925e154ef4b23a5f5d5d4045464bf8ca6ee84dbe
Slava Shpitalny <slavik57@gmail.com>
2017-01-06 16:29:41 +0200

    add class examples

---
+++ src/classesIntro.js
@@ -0,0 +1,49 @@
+var Car = (function () {
+    function Car(brandName) {
+        this.brandName = brandName;
+        Car.SERIES_NUMBER++;
+        this._seriesNumber = Car.SERIES_NUMBER;
+    }
+    Car.BuildCar = function (brandName) {
+        return new Car(brandName);
+    };
+    Car.BuyCar = function (brandName, ownerName) {
+        var car = new Car(brandName);
+        car.ownerName = ownerName;
+        return car;
+    };
+    Car.prototype.startCar = function () {
+        this._startEngine();
+    };
+    Car.prototype._startEngine = function () {
+    };
+    Object.defineProperty(Car.prototype, "seriesNumber", {
+        get: function () {
+            return this._seriesNumber;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Car.prototype, "ownerName", {
+        get: function () {
+            return this._ownerName;
+        },
+        set: function (name) {
+            // Do some validation on the name..
+            this._ownerName = name;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Car;
+}());
+Car.SERIES_NUMBER = 0;
+var car = Car.BuyCar('Mazda', 'Slava');
+car.startCar();
+console.log('Brand number:', car.brandName);
+console.log('Series number:', car.seriesNumber);
+console.log('Owner name:', car.ownerName);
+var car2 = Car.BuyCar('Mazda', 'Other name');
+console.log('Brand number 2:', car2.brandName);
+console.log('Series number 2:', car2.seriesNumber);
+console.log('Owner name:', car2.ownerName);
---
+++ src/classesIntro.ts
@@ -0,0 +1,55 @@
+class Car {
+    private static SERIES_NUMBER = 0;
+
+    private _seriesNumber: number;
+
+    private _ownerName: string;
+
+    private constructor(public brandName: string){
+        Car.SERIES_NUMBER++;
+        this._seriesNumber = Car.SERIES_NUMBER;
+    }
+
+    public static BuildCar(brandName: string): Car {
+        return new Car(brandName);
+    }
+
+    public static BuyCar(brandName: string,
+        ownerName: string): Car{
+        let car = new Car(brandName);
+        car.ownerName = ownerName;
+
+        return car;
+    }
+
+    public startCar(): void {
+        this._startEngine();
+    }
+
+    private _startEngine(): void {
+    }
+
+    public get seriesNumber(): number {
+        return this._seriesNumber;
+    }
+
+    public get ownerName(): string{
+        return this._ownerName;
+    }
+
+    public set ownerName(name: string) {
+        // Do some validation on the name..
+        this._ownerName = name;
+    }
+}
+
+let car = Car.BuyCar('Mazda', 'Slava');
+car.startCar();
+console.log('Brand number:', car.brandName);
+console.log('Series number:', car.seriesNumber);
+console.log('Owner name:', car.ownerName);
+
+let car2 = Car.BuyCar('Mazda', 'Other name');
+console.log('Brand number 2:', car2.brandName);
+console.log('Series number 2:', car2.seriesNumber);
+console.log('Owner name:', car2.ownerName);
