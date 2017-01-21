86d8d25ed1282c73e2c02a8bc8c94554fa274b8f
Slava Shpitalny <slavik57@gmail.com>
2017-01-21 17:47:21 +0200

    add advanced interfaces

---
+++ src/advancedInterfaces.js
@@ -0,0 +1,75 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var Item = (function () {
+    function Item(id) {
+        this.id = id;
+    }
+    return Item;
+}());
+var ItemsCache = (function () {
+    function ItemsCache(itemsConstructor) {
+        this.itemsConstructor = itemsConstructor;
+        this.itemsCache = {};
+    }
+    ItemsCache.prototype.createItem = function (id) {
+        if (!!this.itemsCache[id]) {
+            return this.itemsCache[id];
+        }
+        var item = new this.itemsConstructor(id);
+        this.itemsCache[id] = item;
+        return item;
+    };
+    return ItemsCache;
+}());
+var cache = new ItemsCache(Item);
+var item1 = cache.createItem('a');
+var item2 = cache.createItem('b');
+var item3 = cache.createItem('a');
+console.log('item1:', item1);
+console.log('item2:', item2);
+console.log('item3:', item3);
+console.log('item1 === item2:', item1 === item2);
+console.log('item1 === item3:', item1 === item3);
+var item4 = {
+    id: 'new id'
+};
+console.log('item4:', item4);
+var item5 = {
+    id: 'new id2'
+};
+console.log('item5:', item5);
+console.log('============');
+console.log('============');
+console.log('============');
+var Control = (function () {
+    function Control() {
+    }
+    Control.prototype.doStuff = function (control) {
+        control.state = 'some state';
+    };
+    return Control;
+}());
+var CheckBox = (function (_super) {
+    __extends(CheckBox, _super);
+    function CheckBox() {
+        return _super.apply(this, arguments) || this;
+    }
+    CheckBox.prototype.select = function () {
+    };
+    return CheckBox;
+}(Control));
+var Button = (function () {
+    function Button() {
+    }
+    Button.prototype.select = function () {
+    };
+    return Button;
+}());
+var selectableControl;
+selectableControl = new CheckBox();
+// These will yield errors:
+// selectableControl = new Button();
+//selectableControl.doStuff(new Button()); 
---
+++ src/advancedInterfaces.ts
@@ -0,0 +1,91 @@
+interface ItemConstructor {
+    new (id: string): IItem
+}
+
+interface IItem {
+    id: string;
+}
+
+class Item implements IItem {
+    constructor(public id: string) {
+    }
+}
+
+interface ItemIdToItemMap {
+    [id: string]: IItem;
+}
+
+class ItemsCache {
+    private itemsCache: ItemIdToItemMap;
+
+    constructor(
+        private itemsConstructor: ItemConstructor) {
+        this.itemsCache = {};
+    }
+
+    public createItem(id: string): IItem {
+        if (!!this.itemsCache[id]) {
+            return this.itemsCache[id];
+        }
+
+        const item: IItem =
+            new this.itemsConstructor(id);
+
+        this.itemsCache[id] = item;
+        return item;
+    }
+}
+
+const cache = new ItemsCache(Item);
+const item1 = cache.createItem('a');
+const item2 = cache.createItem('b');
+const item3 = cache.createItem('a');
+
+console.log('item1:', item1);
+console.log('item2:', item2);
+console.log('item3:', item3);
+
+console.log('item1 === item2:', item1 === item2);
+console.log('item1 === item3:', item1 === item3);
+
+const item4: Item = {
+    id: 'new id'
+}
+console.log('item4:', item4);
+
+const item5 = <Item>{
+    id: 'new id2'
+}
+console.log('item5:', item5);
+
+console.log('============');
+console.log('============');
+console.log('============');
+
+class Control {
+    private state: any;
+    doStuff(control: SelectableControl): void {
+        control.state = 'some state';
+    }
+}
+
+interface SelectableControl extends Control {
+    select(): void;
+}
+
+class CheckBox extends Control {
+    select(): void {
+    }
+}
+
+class Button {
+    select(): void {
+    }
+}
+
+let selectableControl: SelectableControl;
+selectableControl = new CheckBox();
+
+// These will yield errors:
+// selectableControl = new Button();
+//selectableControl.doStuff(new Button());
