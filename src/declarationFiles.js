ea904606db86b04008017fada7b1d18e3c18f664
Slava Shpitalny <slavik57@gmail.com>
2017-04-21 15:57:31 +0300

    add declaration files episode

---
+++ src/declarationFiles.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var _ = require("lodash");
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var result = _.union(arr1, arr2);
+console.log(result);
---
+++ src/declarationFiles.ts
@@ -0,0 +1,8 @@
+import * as _ from 'lodash';
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const result = _.union(arr1, arr2);
+
+console.log(result);
